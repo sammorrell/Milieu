dc31851cd83a40c811a5eda58f29c22baf286416
Sam Morrell <samuelmorrell@gmail.com>
2025-05-03 20:42:19 +0100

    Initial Commit

---
+++ MilieuTests/MilieuTests.swift
@@ -0,0 +1,16 @@
+//
+//  MilieuTests.swift
+//  MilieuTests
+//
+//  Created by Sam Morrell on 03/05/2025.
+//
+
+import Testing
+
+struct MilieuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
